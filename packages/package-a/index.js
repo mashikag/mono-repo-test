b029293ae7033d76d9bd4c1b12358c9175242e1e
Maciej Grabowski <littlesoldiergraba@gmail.com>
2024-10-14 13:57:11 +0200

    ads

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,5 +1,6 @@
 module.exports = () => {
   // This is the code that will be executed when the package is imported
   // This is the code that will be executed when the package is imported
+  // This is the code that will be executed when the package is imported
   console.log("Package A was executed");
 };
