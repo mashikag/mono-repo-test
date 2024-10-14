8dc22f45381b5c0f32d304a68363e2ce3b3e9623
Maciej Grabowski <littlesoldiergraba@gmail.com>
2024-10-14 14:01:09 +0200

    Clean up

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -1,6 +1,4 @@
 module.exports = () => {
   // This is the code that will be executed when the package is imported
-  // This is the code that will be executed when the package is imported
-  // This is the code that will be executed when the package is imported
   console.log("Package A was executed");
 };
